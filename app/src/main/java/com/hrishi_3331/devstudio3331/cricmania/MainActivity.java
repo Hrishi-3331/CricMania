7b73fcae520f21ad0d695728c23ea5c3b97ec34a
Hrishi-3331 <hrishikeshugle3331@gmail.com>
2019-05-27 18:35:49 +0530

    Installed Firebase, added Splash screen activity

---
+++ app/src/main/java/com/hrishi_3331/devstudio3331/cricmania/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hrishi_3331.devstudio3331.cricmania;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
