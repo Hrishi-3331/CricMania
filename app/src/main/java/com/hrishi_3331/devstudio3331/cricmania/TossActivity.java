50eb70f2b6a0bf2acf85b28fe246989b6ccb4438
Hrishi-3331 <hrishikeshugle3331@gmail.com>
2019-05-29 16:18:44 +0530

    Still working on toss

--- app/src/main/java/com/hrishi_3331/devstudio3331/cricmania/TossActivity.java
+++ app/src/main/java/com/hrishi_3331/devstudio3331/cricmania/TossActivity.java
@@ -3,23 +3,25 @@ package com.hrishi_3331.devstudio3331.cricmania;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Random;
 
 public class TossActivity extends AppCompatActivity {
 
@@ -34,6 +36,9 @@ public class TossActivity extends AppCompatActivity {
     private String player_1, player_2;
     private TextView wait_bar;
     private boolean ready;
+    private FrameLayout toss_frame;
+    private ImageView toss_head, toss_tail;
+    private String toss_choice_st;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +60,16 @@ public class TossActivity extends AppCompatActivity {
         guest_status = (TextView)findViewById(R.id.guest_status);
         toss_choice = (ImageView)findViewById(R.id.guest_choice);
         wait_bar = (TextView)findViewById(R.id.wait_bar);
+        toss_frame = (FrameLayout)findViewById(R.id.toss_frame);
+        toss_head = (ImageView)findViewById(R.id.frame_heads);
+        toss_tail = (ImageView)findViewById(R.id.frame_tails);
         ready = false;
+
+        toss_frame.setVisibility(View.GONE);
+
+        jRef.child("toss").child("status").setValue("pending");
+        jRef.child("toss").child("res").setValue("pending");
+        jRef.child("toss").child("winner").setValue("pending");
 
         if (isHost){
             DatabaseReference hRef = jRef.child("player1").child("status");
@@ -229,20 +243,35 @@ public class TossActivity extends AppCompatActivity {
     public void checkAvailability(){
 
         if (isHost){
-            if (guest_status.getText().toString().equals(":  online")){
+            if (guest_status.getText().toString() == ":  online"){
                 startGamePlay();
             }
             else {
-                checkAvailability();
+                Handler handler = new Handler();
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        checkAvailability();
+                    }
+                };
+                handler.postDelayed(runnable, 2000);
             }
         }
 
         else {
-            if (host_status.getText().toString().equals(":  online")){
+            if (host_status.getText().toString() == ":  online"){
                 startGamePlay();
             }
             else {
-                checkAvailability();
+                Handler handler = new Handler();
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        checkAvailability();
+                    }
+                };
+                handler.postDelayed(runnable, 2000);
+
             }
         }
 
@@ -250,13 +279,167 @@ public class TossActivity extends AppCompatActivity {
 
     public void startGamePlay(){
         wait_bar.setVisibility(View.GONE);
+        jRef.child("toss").child("status").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String status = dataSnapshot.getValue().toString().trim();
+                if (status != "pending"){
+                    if (status == "heads"){
+                        toss_choice_st = "heads";
+                        toss_choice.setImageResource(R.drawable.heads);
+                    }
+                    else {
+                        toss_choice_st = "tails";
+                        toss_choice.setImageResource(R.drawable.tails);
+                    }
+                    startToss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        jRef.child("toss").child("res").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String res = dataSnapshot.getValue().toString();
+
+                if (res != "pending"){
+                    if(res == toss_choice_st){
+                        jRef.child("toss").child("winner").setValue("player2");
+                    }
+                    else {
+                        jRef.child("toss").child("winner").setValue("player1");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        jRef.child("toss").child("winner").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String winner = dataSnapshot.getValue().toString();
+                if (winner != "pending"){
+                    if (winner == "player1"){
+                        if (isHost){
+                            Toast.makeText(TossActivity.this, "Your opponent lost the toss", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(TossActivity.this, "You lost the toss", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }else {
+                        if (isHost){
+                            Toast.makeText(TossActivity.this, "Your opponent won the toss", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(TossActivity.this, "You won the toss", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
         if (isHost){
-            //Toast.makeText(this, "I am a host", Toast.LENGTH_SHORT).show();
+
         }
         else {
            chooseToss();
         }
     }
+
+    public void startToss() {
+        toss_frame.setVisibility(View.VISIBLE);
+        toss_anim();
+    }
+
+    public void toss_anim() {
+
+            toss_head.setAlpha(0f);
+            toss_tail.setAlpha(0f);
+
+            toss_head.animate().alpha(1f).setDuration(1500).withEndAction(new Runnable() {
+                @Override
+                public void run() {
+                    toss_head.animate().alpha(0f).setDuration(1500).withEndAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            toss_tail.animate().alpha(1f).setDuration(1500).withEndAction(new Runnable() {
+                                @Override
+                                public void run() {
+                                    toss_tail.animate().alpha(0f).setDuration(1500).withEndAction(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            toss_head.animate().alpha(1f).setDuration(1500).withEndAction(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    toss_head.animate().alpha(0f).setDuration(1500).withEndAction(new Runnable() {
+                                                        @Override
+                                                        public void run() {
+                                                            toss_tail.animate().alpha(1f).setDuration(1500).withEndAction(new Runnable() {
+                                                                @Override
+                                                                public void run() {
+                                                                    toss_tail.animate().alpha(1f).setDuration(1500).withEndAction(new Runnable() {
+                                                                        @Override
+                                                                        public void run() {
+                                                                            toss_tail.animate().alpha(0f).setDuration(1500).withEndAction(new Runnable() {
+                                                                                @Override
+                                                                                public void run() {
+                                                                                    if(!isHost){
+                                                                                        generateTossResult();
+                                                                                    }
+
+                                                                                }
+                                                                            }).start();
+                                                                        }
+                                                                    }).start();
+                                                                }
+                                                            }).start();
+                                                        }
+                                                    }).start();
+                                                }
+                                            }).start();
+                                        }
+                                    }).start();
+                                }
+                            }).start();
+                        }
+                    }).start();
+                }
+            }).start();
+
+    }
+
+    public void generateTossResult(){
+        toss_head.setAlpha(0f);
+        toss_tail.setAlpha(0f);
+
+        Random random = new Random();
+        boolean val = random.nextBoolean();
+        if (val){
+            toss_head.animate().alpha(1f).setDuration(1500).start();
+            jRef.child("toss").child("res").setValue("heads");
+        }
+        else {
+            toss_head.animate().alpha(1f).setDuration(1500).start();
+            jRef.child("toss").child("res").setValue("tails");
+        }
+    }
+
 
     public void chooseToss(){
         if (ready){
@@ -314,6 +497,7 @@ public class TossActivity extends AppCompatActivity {
             heads.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    jRef.child("toss").child("status").setValue("heads");
                     dialog.dismiss();
                 }
             });
@@ -321,6 +505,7 @@ public class TossActivity extends AppCompatActivity {
             tails.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    jRef.child("toss").child("status").setValue("tails");
                     dialog.dismiss();
                 }
             });
